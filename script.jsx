f2fe453260ae44693c4fde4cbeef691ff26fedc8
ness <duranr@protonmail.com>
2023-01-03 23:16:54 -0500

    Initial Commit

---
+++ script.jsx
@@ -0,0 +1,9 @@
+/*
+ *  After Effects Keyframe Adjust Script
+ *  Author: Ness
+ *  License: MIT
+ * */
+
+// Initialize Window
+var mainWindow = new Window("palette", "Keyframe Move Script")
+mainWindow.show()
